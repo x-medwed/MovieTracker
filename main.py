7fd05d07b7657df93dd751fc545ce53067089caa
x-medwed <x.medwed@gmail.com>
2025-10-05 22:37:00 +0300

    Добавил структуру файлов. Обновил - README.md Добавил шаблон для комитов - .gitmessage.txt
     📁 Структура проєкту:
    - GUI: app/gui/
    - База даних: app/db/
    - Excel-експорт: app/excel/
    - Утиліти: app/utils/
    - Конфігурація: settings.json, config.py
    - Точка входу: main.py
    
    🛠 Технології:
    - Python, PyQt або Tkinter
    - SQLite, pandas, openpyxl
    - Git, GitHub, venv

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-def add(a, b):
-    return a + b
+# main.py
+import tkinter as tk
+from app.db import database
+
+def main():
+    # создаём главное окно
+    root = tk.Tk()
+    root.title("Записник фільмів і серіалів")
+    root.geometry("400x300")
+
+    # базовая метка
+    label = tk.Label(root, text="Вітаю у твоєму записнику!", font=("Arial", 14))
+    label.pack(pady=20)
+
+    # запускаем интерфейс
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
